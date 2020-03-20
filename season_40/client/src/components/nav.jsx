e6284df7fef05d51c51e2994f3436384b2b92dcd
Christian Bueno <christianjongbueno@gmail.com>
2020-03-19 19:01:52 -0700

    Added season select

--- season_40/client/src/components/nav.jsx
+++ season_40/client/src/components/nav.jsx
@@ -9,10 +9,11 @@ const Nav = (props) => {
         <button type="button" className="list-group-item list-group-item-action list-group-item-warning">
           Actions:
         </button>
-        <button type="button" className="list-group-item list-group-item-action" onClick={() => props.showNav('menuHome')}>Home</button>
+        <button type="button" className="list-group-item list-group-item-action" onClick={() => props.showNav('menuHome')}>All Players</button>
         <button type="button" className="list-group-item list-group-item-action" onClick={() => props.showNav('menuTribes')}>Tribes</button>
         <button type="button" className="list-group-item list-group-item-action" onClick={() => props.showNav('menuTrade')}>Make a Trade</button>
         <button type="button" className="list-group-item list-group-item-action" onClick={() => props.showNav('menuEdit')}>Edit Players</button>
+        <button type="button" className="list-group-item list-group-item-action" onClick={() => props.selectionScreen()}>Change Season</button>
       </div>
     </div>
   );
